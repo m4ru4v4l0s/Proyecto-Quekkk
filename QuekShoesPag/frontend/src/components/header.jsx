ad699e6dfb8f8721a28edb7c54306a43473d19f0
m4ru4v4l0s <Marvalen1124@gmail.com>
2025-11-07 14:16:31 -0300

    acomodando el header

--- QuekShoesPag/frontend/src/components/header.jsx
+++ QuekShoesPag/frontend/src/components/header.jsx
@@ -1,10 +1,12 @@
  import '../styles/header.css'
  import carrito from "../../public/carrito.png"
+ import logo from "../../public/logo.png"
+ import headerz from "../../public/zapasheader.png"
  export default function Header (){
     return (
         <header className="header">
     <div className="menu-container">
-
+        <img src= {logo} className='img-logo' alt=''/>
       <nav className="nav">
         <ul>
           <li><a href="#">Inicio</a></li>
@@ -15,31 +17,14 @@
       </nav>
 
       <div>
-        <ul>
-          <li className="carrito-container">
-            <img src={carrito} id="img-carrito" alt="carrito" />
-            <div id="carrito">
-              <table id="lista-carrito">
-                <thead>
-                  <tr>
-                    <th>Imagen</th>
-                    <th>Nombre</th>
-                    <th>Precio</th>
-                  </tr>
-                </thead>
-                <tbody></tbody>
-              </table>
-
-              <a href="#" id="vaciar-carrito" class="btn-vaciar">Vaciar Carrito</a>
-            </div>
-          </li>
-        </ul>
+            <img src={carrito} id="img-carrito" alt="carrito" /> 
       </div>
+      
     </div>
 
     <div class="header-zapatillas">
       <div class="header-img">
-        <img src="../assets/assets/menu/zapasheader.png" alt="" />
+        <img src={headerz} alt="" />
       </div>
 
       <div class="texto-header">
